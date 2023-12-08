b8dd21c138cbc26bb6044654e7d2091d65abf5eb
Nathan Garcia <n8g423@gmail.com>
2023-12-08 15:43:40 -0700

    Update styling

--- src/components/item-list/item-list.jsx
+++ src/components/item-list/item-list.jsx
@@ -1,47 +1,49 @@
 // import { stringCleaner } from "@/utils/util";
-import './item-list.css'
+import "./item-list.css";
 
+export default function ItemList({ data = [] }) {
+  function build_Header() {
+    return Object.keys(data[0]).map((el, i) => (
+      <th
+        className={"font-bold text-s text-gray-700 uppercase px-6 py-3"}
+        key={i}
+      >
+        {" "}
+        {el}{" "}
+      </th>
+    ));
+  }
 
-export default function ItemList ({ data = [] }) {
+  function build_Body() {
+    return data.map((student, i) => {
+      return (
+        <tr key={i}>
+          {Object.values(student).map((el, i) => {
+            return (
+              <td className={"px-6 py-3 "} key={i}>
+                {" "}
+                {el.toString()}{" "}
+              </td>
+            );
+          })}
+        </tr>
+      );
+    });
+  }
 
-	function build_Header() {
-		return Object.keys( data[0] ).map(( el,i ) =>
-					<th className={'px-6 py-3'} key={i}> { el } </th>
-				)
-	}
+  return (
+    <div
+      className={
+        "relative overflow-x-auto sm:rounded-lg flex flex-col items-center justify-center"
+      }
+    >
+      <table className={"table-auto w-full text-sm text-left rtl:text-right"}>
+        <tbody>
+          <tr className={"capitalize"}>{build_Header()}</tr>
 
-	function build_Body() {
-		return (
-			( data ).map(( student,i ) => {
-				return (
-					<tr key={i}>
-						{
-							Object.values( student ).map(( el,i ) => {
-								return <td className={'px-6 py-3'} key={i}> { el.toString() } </td>
-							})
-						}
-					
-					</tr>
-			)})
-		)
-	}
-
-
-	return (
-		<div className={'item-list-component flex flex-col gap-4 font-normal'}>
-
-			<table className={'table-auto'}>
-				<tbody>
-
-					<tr className={'capitalize'}>
-						{ build_Header() }
-					</tr>
-
-					{ build_Body() }
-
-				</tbody>
-			</table>
-
-		</div>
-	);
-};
+          {build_Body()}
+        </tbody>
+      </table>
+    </div>
+  );
+}
